9a6f3c0d73b053eea1790cedbfd30df49b1e89d6
sumitsharma16 <52009661+sumitsharma16@users.noreply.github.com>
2022-06-19 13:23:36 +0530

    Update Workflow.groovy

--- vars/Workflow.groovy
+++ vars/Workflow.groovy
@@ -1,5 +1,8 @@
-def call(Map parameter){
-  node{
+def call(Map parameter) {
     
-
+     bitbucket.scm(
+        stageName: "checkout SCM",
+        GIT_URL:   "${config.GIT_URL}",
+        BRANCH:    "${config.BRANCH}"
+    )
 }
