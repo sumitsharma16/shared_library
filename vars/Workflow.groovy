d896902a24c12c4afdd9a469d696d9ceff35a4c6
sumitsharma16 <52009661+sumitsharma16@users.noreply.github.com>
2022-06-19 13:56:59 +0530

    Update Workflow.groovy

--- vars/Workflow.groovy
+++ vars/Workflow.groovy
@@ -3,6 +3,6 @@ def call(Map parameter) {
      bitbucket.scm(
         stageName: "checkout SCM",
         GIT_URL:   "${config.GIT_URL}",
-        BRANCH:    "${config.BRANCH}"
+        TAG:    "${config.TAG}"
     )
 }
